4c50a752d4f40dc509e4b15ee8c265eedb4d7148
SaberSaied <sabersaied992003@gmail.com>
2026-05-04 22:59:59 +0300

    feat(network): setup base http client

---
+++ apps/mobile/lib/core/network/dio_client.dart
@@ -0,0 +1,64 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import '../constants/api_constants.dart';
+import 'auth_interceptor.dart';
+
+class DioClient {
+  late final Dio _dio;
+
+  DioClient({required AuthInterceptor authInterceptor}) {
+    _dio = Dio(
+      BaseOptions(
+        baseUrl: ApiConstants.baseUrl,
+        connectTimeout: const Duration(seconds: 15),
+        receiveTimeout: const Duration(seconds: 15),
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+      ),
+    );
+
+    _dio.interceptors.addAll([
+      authInterceptor,
+      if (kDebugMode)
+        LogInterceptor(
+          requestBody: true,
+          responseBody: true,
+          logPrint: (obj) => debugPrint(obj.toString()),
+        ),
+    ]);
+  }
+
+  Dio get dio => _dio;
+
+  // GET
+  Future<Response> get(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+  }) async {
+    return _dio.get(path, queryParameters: queryParameters);
+  }
+
+  // POST
+  Future<Response> post(
+    String path, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+  }) async {
+    return _dio.post(path, data: data, queryParameters: queryParameters);
+  }
+
+  // PATCH
+  Future<Response> patch(
+    String path, {
+    dynamic data,
+  }) async {
+    return _dio.patch(path, data: data);
+  }
+
+  // DELETE
+  Future<Response> delete(String path) async {
+    return _dio.delete(path);
+  }
+}
